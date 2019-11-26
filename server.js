5fd9d7fa298bb0a5dab2cb8800ee9dc03c127cc3
Muttakin Islam <islammuttakin@live.com>
2019-11-26 13:51:58 +0600

    new server code3.0

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var http = require('http');
 var express = require('express');
-var app = express();
+var server = express();
 var server = http.Server(server);
 var bodyParser = require('body-parser');
 var mongo = require('mongodb');
 
 var path = require('path');
-app.use(express.static(path.join(__dirname,'/')))
+server.use(express.static(path.join(__dirname,'/')))
 var db_url = "mongodb+srv://Muttakin:12345six@cluster0-2wehj.mongodb.net/test?retryWrites=true&w=majority";
 
 

fd795add891f644fa7173ccefdde74c5d537f54e
Muttakin Islam <islammuttakin@live.com>
2019-11-27 18:12:54 +0600

    updated database

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var mongo = require('mongodb');
 
 var path = require('path');
 app.use(express.static(path.join(__dirname,'/')))
-var db_url = "mongodb+srv://Muttakin:12345six@cluster0-2wehj.mongodb.net/test?retryWrites=true&w=majority";
+var db_url = "mongodb+srv://Muttakin:muttakin12@cluster1-2wehj.mongodb.net/test?retryWrites=true&w=majority";
 
 
 var mongoose = require("mongoose");

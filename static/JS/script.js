96f1d4877287e54b39ad2d377275894faab5f89a
Muttakin Islam <islammuttakin@live.com>
2019-11-30 13:28:28 +0600

    pdf generation with comments

--- static/JS/script.js
+++ static/JS/script.js
@@ -106,12 +106,7 @@ function divide(){  //START OF JS
     });
     
    
-    
 
-  //});
-
-// Variant
-// This one lets you improve the PDF sharpness by scaling up the HTML node tree to render as an image before getting pasted on the PDF.
 
   var newCanvas = document.querySelector('#myChart');
   var newCanvas1 = document.querySelector('#myChart1');
